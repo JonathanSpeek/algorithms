551cc76eb612a67d06e8b22bf03939c5b2cf66cc
jonathanspeek <jonspeek@gmail.com>
2017-05-01 10:21:24 -0600

    Added recusrive implemnation of binary search in Python

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -1,8 +1,8 @@
-# implementation of binary search in Python
+# iterative implementation of binary search in Python
 
 
 def binary_search(a_list, item):
-    """Performs binary search to find the position of an integer in a given, sorted, list.
+    """Performs iterative binary search to find the position of an integer in a given, sorted, list.
 
     a_list -- sorted list of integers
     item -- integer you are searching for the position of
@@ -21,4 +21,30 @@ def binary_search(a_list, item):
         elif a_list[i] < item:
             first = i + 1
         else:
-            return '{item} not found in this list'.format(item=item)
+            return '{item} not found in the list'.format(item=item)
+
+
+# recursive implementation of binary search in Python
+
+
+def binary_search_recursive(a_list, item):
+    """Performs recursive binary search of an integer in a given, sorted, list.
+
+    a_list -- sorted list of integers
+    item -- integer you are searching for the position of
+    """
+
+    first = 0
+    last = len(a_list) - 1
+
+    if len(a_list) == 0:
+        return '{item} was not found in the list'.format(item=item)
+    else:
+        i = (first + last) // 2
+        if item == a_list[i]:
+            return '{item} found'.format(item=item)
+        else:
+            if a_list[i] < item:
+                return binary_search_recursive(a_list[i+1:], item)
+            else:
+                return binary_search_recursive(a_list[:i], item)
