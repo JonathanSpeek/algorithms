b2f4bbb712742bb716dff25b99243008a05d9d49
Jonathan Speek <jonspeek@gmail.com>
2020-05-30 09:53:18 -0600

    Sprucing-up binary search recursive impl

--- binary_search/binarySearch.js
+++ binary_search/binarySearch.js
@@ -25,21 +25,20 @@ console.log(binarySearch(arr, 9));
 // recursive implementation of binary search in JavaScript
 
 const binarySearchRecursive = (arr, item) => {
-  if (arr.length < 1) return 'Item not found in the arr.';
+  const n = arr.length;
+  const mid = Math.floor((n - 1) / 2);
 
-  let right = arr.length;
-  let left = 0;
-  let middle = Math.floor((left + right) / 2);
+  if (n === 0) return 'Item not found in the arr.';
 
-  if (arr[middle] === item) {
-    return `Found at position ${middle}.`;
+  if (arr[mid] === item) {
+    return true;
   }
 
-  if (arr[middle] < item) {
-    return binarySearchRecursive(array.slice(middle + 1, right), item);
+  if (arr[mid] < item) {
+    return binarySearchRecursive(arr.slice(mid + 1), item);
   } else {
-    return binarySearchRecursive(array.slice(left, middle), item);
+    return binarySearchRecursive(arr.slice(0, mid), item);
   }
 }
 
-console.log(binarySearch(arr, 4));
+console.log(binarySearchRecursive(arr, 6));
