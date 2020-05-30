40b3d6771996b0cb4bf720f97081c557d438d2ac
Jonathan Speek <jonspeek@gmail.com>
2020-05-30 08:10:39 -0600

    Adds recusrive impl. of insertion sort in js

--- insertion_sort/insertionSort.js
+++ insertion_sort/insertionSort.js
@@ -1,8 +1,7 @@
-// implementation of insertion sort in JavaScript on an arr of integers
-
 const arr = [100, 6, 34, 78, 2, 100, 10000, 5, 789];
 
-const insertionSort  = (arr) => {
+// iterative implementation of insertion sort in JS on an arr of integers
+const insertionSort = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     let current = arr[i];
     let j = i;
@@ -16,3 +15,29 @@ const insertionSort  = (arr) => {
 }
 
 console.log(insertionSort(arr));
+
+// recursive implementation of insertion sort in JS on an arr of integers
+const sort = (arr, n) => {
+  if (n <= 1) {
+    return arr;
+  }
+  sort(arr, n - 1);
+
+  for (let i = 0; i < arr.length; i++) {
+    let current = arr[i];
+    let j = i;
+    while (j > 0 && arr[j - 1] > current) {
+      arr[j] = arr[j - 1];
+      j -= 1;
+    }
+    arr[j] = current;
+  }
+  return arr;
+}
+
+const recursiveInsertionSort = (arr) => {
+  const n = arr.length;
+  return sort(arr, n);
+}
+
+console.log(recursiveInsertionSort(arr));
