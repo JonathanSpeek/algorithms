17809bbaf2213ec7dec35ef04d0d1a83c6b5d128
Jonathan Speek <jonspeek@gmail.com>
2020-05-30 07:16:40 -0600

    Adds recursive bubble sort in JS

--- bubble_sort/bubbleSort.js
+++ bubble_sort/bubbleSort.js
@@ -1,4 +1,4 @@
-// implementation of bubble sort in JavaScript on an array of integers
+// iterative implementation of bubble sort in JS on an array of integers
 
 const arr = [100, 6, 34, 78, 2, 100, 10000, 5, 789];
 
@@ -14,3 +14,24 @@ const bubbleSort = (arr) => {
 }
 
 console.log(bubbleSort(arr));
+
+
+// recursive implementation of bubble sort in JS on an array of integers
+const sort = (arr, n) => {
+  if (n === 1) {
+    return arr;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i + 1]) {
+      [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+    }
+  }
+  return sort(arr, n - 1);
+};
+
+const recursiveBubbleSort = (vector) => {
+  const n = vector.length - 1;
+  return sort(vector, n);
+}
+
+console.log(recursiveBubbleSort(arr, arr.length - 1));
